044d0f4592e6fd67be76934a3cc7226c350cded5
Isaac <miguel.isaac@live.com>
2023-09-19 11:58:27 -0600

    Creando register

---
+++ src/app/register/register-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RegisterRoutingModule { }
